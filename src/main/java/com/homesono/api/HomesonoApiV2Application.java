ff5c843ca17d0321de0bbede5282dc2e39ab0097
Nicolas Maes <contact@nicolasmaes.fr>
2023-08-07 22:31:12 +0200

    first commit

---
+++ src/main/java/com/homesono/api/HomesonoApiV2Application.java
@@ -0,0 +1,13 @@
+package com.homesono.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesonoApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesonoApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homesono/api/HomesonoApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homesono.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesonoApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
